d47d21b89df6d66fda728386075ef1a06730a23b
bumerrrang <andrei.lunin.0@gmail.com>
2026-09-09 11:25:06 +0300

    Lesson 7: Using ngmodel to bind values to form controls

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,16 +9,24 @@ import { WishItem } from '../shared/models/wishItem';
   templateUrl: './app.html',
 })
 export class App {
-//  protected readonly title = signal('wishlist-3');
-  items : WishItem[] = [
-    new WishItem('Learn Angular'),
-    new WishItem('Get Coffee', true),
-    new WishItem('Find grass that cuts itself')
+  //  protected readonly title = signal('wishlist-3');
+  items: WishItem[] = [
+    // new WishItem('Learn Angular'),
+    // new WishItem('Get Coffee', true),
+    // new WishItem('Find grass that cuts itself'),
   ];
+
+  newWishText = '';  // user inputs new wish item
+
+  protected addNewWish() {
+    console.log('Adding new wish');
+    this.items.push(new WishItem(this.newWishText)); // adding new item to the collection
+    this.newWishText = ''; // clear the textbox
+  }
 
   protected toggleItem(e: WishItem) {
     console.log('clicked!');
     console.log(e);
-    e.isComplete = !e.isComplete;  // toggling by hand, as there is only one way binding
+    e.isComplete = !e.isComplete; // toggling by hand, as there is only one way binding
   }
 }
